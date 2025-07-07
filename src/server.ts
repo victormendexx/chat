51a195aa637ed8b50a76755b4bf8e97c391fcceb
Rafael Mendes <rafael.mendes.negocios@gmail.com>
2025-07-07 10:56:27 -0300

    definição de quem mandou cada imagem

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,14 @@ class App {
 
       socket.on("message", (msg) => {
         console.log("chegou mensagem:", msg);
-        socket.broadcast.emit("message", msg);
+        const data = {
+          id: socket.id,
+          message: msg,
+        };
+        socket.broadcast.emit("message", {
+          id: socket.id,
+          message: msg,
+        });
       });
     });
   }
